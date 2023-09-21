a0e7db324d5b5f2aff16227acf109c3f88e84f50
miyamoto <miyamoto.git@gmail.com>
2023-09-21 18:21:11 +0900

    マウスによるカードのドラッグ処理を実装

--- main.gd
+++ main.gd
@@ -2,15 +2,28 @@ extends Node2D
 
 @onready var card: Card = $Card
 
+var dragged_card: Card = null
 
 func _process(delta)->void:
-	check_left_click()
+	input_left_click()
+	drag_card()
 
 
-func check_left_click():
+func input_left_click():
 	var mouse_position := get_global_mouse_position()
 	
 	if Input.is_action_just_pressed("left_click"):
 		if card.check_collision(mouse_position):
-			print("click")
+			dragged_card = card
+			dragged_card.set_clicked_local_position(mouse_position)
+	if Input.is_action_just_released("left_click") && dragged_card != null:
+			dragged_card = null
+		
+
+func drag_card()->void:
+	if dragged_card == null:
+		return
+	
+	dragged_card.dragged_global_position(get_global_mouse_position())
+	
 		
--- scenes/card.gd
+++ scenes/card.gd
@@ -5,18 +5,30 @@ extends Node2D
 const CARD_WIDTH: int = 40
 const CARD_HEIGHT: int = 50
 
+var _clicked_local_position: Vector2 = Vector2.ZERO
 
-func check_collision(object_position: Vector2)->bool:
-	if object_position.x < global_position.x - CARD_WIDTH/2.0:
+
+
+func check_collision(mouse_global_position: Vector2)->bool:
+	if mouse_global_position.x < global_position.x - CARD_WIDTH/2.0:
 		return false
 		
-	if  global_position.x + CARD_WIDTH/2.0 < object_position.x:
+	if  global_position.x + CARD_WIDTH/2.0 < mouse_global_position.x:
 		return false
 		
-	if object_position.y < global_position.y - CARD_HEIGHT/2.0:
+	if mouse_global_position.y < global_position.y - CARD_HEIGHT/2.0:
 		return false
 		
-	if  global_position.y + CARD_HEIGHT/2.0 < object_position.y:
+	if  global_position.y + CARD_HEIGHT/2.0 < mouse_global_position.y:
 		return false
 	
 	return true
+	
+	
+func set_clicked_local_position(mouse_global_position: Vector2):
+	_clicked_local_position = global_position - mouse_global_position
+	
+	
+func dragged_global_position(mouse_global_position: Vector2):
+	global_position = _clicked_local_position + mouse_global_position
+
